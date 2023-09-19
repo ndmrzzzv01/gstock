bd529d6b835a5b670f3fe2d1a4af2f2e4043b01a
Nadia Morozova <ndmorozova01@gmail.com>
2023-09-19 22:36:18 +0200

    Added loading for gifs.

--- app/src/main/java/com/ndmrzzzv/giphystock/fragment/detail/DetailFragment.kt
+++ app/src/main/java/com/ndmrzzzv/giphystock/fragment/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.ndmrzzzv.giphystock.fragment.detail
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.ndmrzzzv.giphystock.databinding.FragmentDetailGifBinding
 import com.ndmrzzzv.giphystock.fragment.screen_features.FullscreenFragment
@@ -49,6 +53,27 @@ class DetailFragment : Fragment() {
             Glide
                 .with(requireContext())
                 .load(gif.url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+                })
                 .apply(
                     RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
                 )
--- app/src/main/java/com/ndmrzzzv/giphystock/fragment/main/view/holder/GifViewHolder.kt
+++ app/src/main/java/com/ndmrzzzv/giphystock/fragment/main/view/holder/GifViewHolder.kt
@@ -1,9 +1,14 @@
 package com.ndmrzzzv.giphystock.fragment.main.view.holder
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.ndmrzzzv.domain.model.Gif
 import com.ndmrzzzv.giphystock.databinding.ItemGifBinding
@@ -17,6 +22,27 @@ class GifViewHolder(
             Glide
                 .with(imgGif.context)
                 .load(gif.url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+                })
                 .transform(RoundedCorners(24))
                 .apply(
                     RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
