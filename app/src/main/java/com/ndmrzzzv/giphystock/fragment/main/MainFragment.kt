5341ebe1336782fd68fe15532cdf80237f57d144
Nadia Morozova <ndmorozova01@gmail.com>
2023-09-20 10:08:47 +0200

    Cleaned up and tested code.

--- app/src/main/java/com/ndmrzzzv/giphystock/fragment/main/MainFragment.kt
+++ app/src/main/java/com/ndmrzzzv/giphystock/fragment/main/MainFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.ndmrzzzv.giphystock.databinding.FragmentMainListBinding
+import com.ndmrzzzv.giphystock.fragment.features.FullscreenFragment
 import com.ndmrzzzv.giphystock.fragment.main.view.adapter.GifAdapter
 import com.ndmrzzzv.giphystock.fragment.main.view.interfaces.NavigateToGifListener
-import com.ndmrzzzv.giphystock.fragment.features.FullscreenFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment(), NavigateToGifListener {
